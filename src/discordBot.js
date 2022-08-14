73492bbeba6459c25cbae19f0803a6e4d5414cf2
Patrick Oyardo <patrick@smartwave.ph>
2022-08-14 21:41:45 +0800

    test heroku

--- src/discordBot.js
+++ src/discordBot.js
@@ -12,7 +12,7 @@ const client = new Discord.Client({
 })
 
 //Login
-client.login(process.env.DISCORDJS_BOT_TOKEN)
+client.login(process.env.DISCORDJS_BOT_TOKEN) 
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`)
