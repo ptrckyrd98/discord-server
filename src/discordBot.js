e9e0dcbd3db9a959888d8b2238471e31cd1edac8
Patrick Oyardo <patrick@smartwave.ph>
2022-08-14 18:05:06 +0800

    Add new feature automatic  Welcoming Bot to our new member of the server

--- src/discordBot.js
+++ src/discordBot.js
@@ -1,7 +1,24 @@
 require('dotenv').config()
 
-const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const Discord = require('discord.js')
+const client = new Discord.Client({ 
+    intents: [
+        'GUILDS', 
+        'GUILD_MESSAGES', 
+        'GUILD_MEMBERS'
+    ]
+})
 
 //Login
 client.login(process.env.DISCORDJS_BOT_TOKEN)
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`)
+})
+
+//Welcome
+client.on('guildMemberAdd', (member) => {
+    const anncouncementChannelId = process.env.ANNOUNCEMENT_CH_ID
+    const generalChannelId = process.env.GENERAL_CH_ID
+    member.guild.channels.cache.get(anncouncementChannelId).send(`Hey <@${member.id}>, welcome to the server! Don't be shy. Head to the <#${generalChannelId}> and introduce yourself!`)
+})
