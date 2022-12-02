86a4f303f1f8d9354ccf160c09f30af6c15210ad
Patrick Heinz Oyardo <patrick@smartwave.ph>
2022-12-02 15:05:17 +0800

    Remove Automatic Message

--- src/discordBot.js
+++ src/discordBot.js
@@ -35,31 +35,31 @@ client.on('guildMemberAdd', async (member) => {
 })
 
 //Morning Message
-const morningMsg = schedule.scheduleJob({ hour: 7, minute: 59 }, () => {
-    client.channels.cache.get(generalChannelId).send('Good Morning! @everyone')
-    client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/YvhU61Lel3LVZTAfGO/giphy-downsized-large.gif')
-})
+// const morningMsg = schedule.scheduleJob({ hour: 7, minute: 59 }, () => {
+//     client.channels.cache.get(generalChannelId).send('Good Morning! @everyone')
+//     client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/YvhU61Lel3LVZTAfGO/giphy-downsized-large.gif')
+// })
 
 // Afternoon Message
-const afternoonMsg = schedule.scheduleJob({ hour: 11, minute: 59 }, () => {
-    client.channels.cache.get(generalChannelId).send('Good Afternoon! @everyone, Lunch Time')
-    client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/d2ItDZZumUI6Y/giphy.gif')
-})
+// const afternoonMsg = schedule.scheduleJob({ hour: 11, minute: 59 }, () => {
+//     client.channels.cache.get(generalChannelId).send('Good Afternoon! @everyone, Lunch Time')
+//     client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/d2ItDZZumUI6Y/giphy.gif')
+// })
 
 // Evening Message
-const eveningMsg = schedule.scheduleJob({ hour: 6, minute: 30 }, () => {
-    client.channels.cache.get(generalChannelId).send('Good Evening! @everyone, how\'s your day? I hope you have a great day!')
-    client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/mWGkvExueCYKX0o4il/giphy.gif')
-})
+// const eveningMsg = schedule.scheduleJob({ hour: 6, minute: 30 }, () => {
+//     client.channels.cache.get(generalChannelId).send('Good Evening! @everyone, how\'s your day? I hope you have a great day!')
+//     client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/mWGkvExueCYKX0o4il/giphy.gif')
+// })
 
 // Evening Message
-const weekEndMsg = schedule.scheduleJob({ hour: 6, minute: 30, dayOfWeek: 5 }, () => {
-    client.channels.cache.get(generalChannelId).send('Hello! @everyone, Happy Weekend Y\'All!')
-    client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/cKbLJzS0cOTklEmzto/giphy.gif')
-})
+// const weekEndMsg = schedule.scheduleJob({ hour: 6, minute: 30, dayOfWeek: 5 }, () => {
+//     client.channels.cache.get(generalChannelId).send('Hello! @everyone, Happy Weekend Y\'All!')
+//     client.channels.cache.get(generalChannelId).send('https://media.giphy.com/media/cKbLJzS0cOTklEmzto/giphy.gif')
+// })
 
 //Monday Feels
-const mondayFeelMsg = schedule.scheduleJob({ hour: 20, minute: 30, dayOfWeek: 0 }, () => {
-    client.channels.cache.get(generalChannelId).send('Shet! Monday na ulet bukas.')
-    client.channels.cache.get(generalChannelId).send('https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/298984971_1126961094596583_1121420645209888551_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFzX0PhtM83cXZHKzZqIBjQroXNOTV0YS2uhc05NXRhLfNX5UTKsb87OiaLtQwLEqU1cCKhndSfV5LjXF-gyhp5&_nc_ohc=Um90p5a74fMAX_oBfyz&_nc_ht=scontent.fmnl25-4.fna&oh=00_AT8Uj6OCnTPinhcpRW9OXnGH-Wp2XwoAtPwaYlXrBmhpfA&oe=6304B265')
-})
+// const mondayFeelMsg = schedule.scheduleJob({ hour: 20, minute: 30, dayOfWeek: 0 }, () => {
+//     client.channels.cache.get(generalChannelId).send('Shet! Monday na ulet bukas.')
+//     client.channels.cache.get(generalChannelId).send('https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/298984971_1126961094596583_1121420645209888551_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFzX0PhtM83cXZHKzZqIBjQroXNOTV0YS2uhc05NXRhLfNX5UTKsb87OiaLtQwLEqU1cCKhndSfV5LjXF-gyhp5&_nc_ohc=Um90p5a74fMAX_oBfyz&_nc_ht=scontent.fmnl25-4.fna&oh=00_AT8Uj6OCnTPinhcpRW9OXnGH-Wp2XwoAtPwaYlXrBmhpfA&oe=6304B265')
+// })
